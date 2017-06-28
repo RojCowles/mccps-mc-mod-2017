995083aec424edddd88cc63665ae1dd5cb2c5804
Roger Cowles <rojcowles@gmail.com>
2017-06-28 09:57:05 -0400

    Added initial annotations for the Block Break mod
    Added the @EventHandler and @Subscribe event framework

--- src/main/java/org/marbleheadcharter/forge/mods/BlockBreakMessage.java
+++ src/main/java/org/marbleheadcharter/forge/mods/BlockBreakMessage.java
@@ -1,5 +1,13 @@
 package org.marbleheadcharter.forge.mods;
 
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 public class BlockBreakMessage {
+	@SubscribeEvent
+	public void sendMessage(BreakEvent event)
+	{
+		
+	}
 
 }
--- src/main/java/org/marbleheadcharter/forge/mods/Main.java
+++ src/main/java/org/marbleheadcharter/forge/mods/Main.java
@@ -1,5 +1,16 @@
 package org.marbleheadcharter.forge.mods;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "mccpsmods", version = "1.0")
 public class Main {
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
 
 }
